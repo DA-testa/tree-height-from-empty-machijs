1373c26ede753b89613c087405a91fedf66cd019
Ričards Ivars Uldukis 221RDB151 <125198479+machijs@users.noreply.github.com>
2023-03-05 17:51:17 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -24,17 +24,17 @@ def compute_height(n, parents):
 def main():
     task_num = None
     while task_num not in ['F', 'I']:
-        task_num = input().upper()
+        task_num = input().upper().strip()
 
     if task_num == 'F':
-        task_num = input()
+        task_num = input().strip()
         input_file = f"{task_num}"
         if not os.path.exists(input_file):
             print(f"{input_file} neeksistē")
             return
         with open(input_file, 'r') as f:
             n = int(f.readline())
-            parents = np.fromstring(f.readline(), sep=' ', dtype=int)
+            parents = np.fromstring(f.readline().strip(), sep=' ', dtype=int)
             if len(parents) != n:
                 return
         tree_height = compute_height(n, parents)
@@ -45,10 +45,10 @@ def main():
         n = None
         while not isinstance(n, int) or n < 1 or n > 100000:
             try:
-                n = int(input())
+                n = int(input().strip())
             except ValueError:
                 print("ievadiet skaitli")
-        parents_str = input()
+        parents_str = input().strip()
         parents = np.fromstring(parents_str, sep=' ', dtype=int)
         if len(parents) != n:
             return
