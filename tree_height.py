9f482083215e9bf1ad5f6500ba79b1fbf1dabdcf
Ričards Ivars Uldukis 221RDB151 <125198479+machijs@users.noreply.github.com>
2023-03-05 16:02:15 +0200

    Add files via upload

--- tree_height.py
+++ tree_height.py
@@ -1,6 +1,25 @@
 import sys
 import threading
 import numpy as np
+import os
+
+def read_input():
+    while True:
+        input_option = input()
+        if input_option.upper() == 'I':
+            n = int(input())
+            parents = np.fromstring(input(), sep=' ', dtype=int)
+            return n, parents
+        elif input_option.upper() == 'F':
+            filename = input()
+            while 'a' in filename or not os.path.exists(f"tree-height-from-empty-machijs/test/"):
+                filename = input()
+            with open(f"tree-height-from-empty-machijs/test/", 'r') as f:
+                n = int(f.readline())
+                parents = np.fromstring(f.readline(), sep=' ', dtype=int)
+            return n, parents
+        else:
+            print()
 
 def compute_height(n, parents):
     tree = [[] for i in range(n)]
@@ -19,13 +38,10 @@ def compute_height(n, parents):
 
     return height(root)
 
-
 def main():
-    n = int(input())
-    parents = np.fromstring(input(), sep=' ', dtype=int)
+    n, parents = read_input()
     tree_height = compute_height(n, parents)
     print(tree_height)
-
 
 sys.setrecursionlimit(10**9)
 threading.stack_size(2**27)
