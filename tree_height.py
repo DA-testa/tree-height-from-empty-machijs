cc090e3fc81be2b112e74ac1de31620a796c3b8c
Ričards Ivars Uldukis 221RDB151 <125198479+machijs@users.noreply.github.com>
2023-03-05 17:41:40 +0200

    Add files via upload

--- tree_height.py
+++ tree_height.py
@@ -22,19 +22,42 @@ def compute_height(n, parents):
 
 
 def main():
-    task_num = input()
-    if not task_num.isdigit():
-        print("nav skaitlis")
+    task_num = None
+    while task_num not in ['F', 'I']:
+        task_num = input().upper()
+
+    if task_num == 'F':
+        task_num = input()
+        input_file = f"{task_num}"
+        if not os.path.exists(input_file):
+            print(f"{input_file} neeksistē")
+            return
+        with open(input_file, 'r') as f:
+            n = int(f.readline())
+            parents = np.fromstring(f.readline(), sep=' ', dtype=int)
+            if len(parents) != n:
+                return
+        tree_height = compute_height(n, parents)
+        print(tree_height) 
+
+
+    elif task_num == 'I':
+        n = None
+        while not isinstance(n, int) or n < 1 or n > 100000:
+            try:
+                n = int(input("Enter number of nodes (1-100000): "))
+            except ValueError:
+                print("Error: number of nodes must be a positive integer!")
+        parents_str = input("Enter parent nodes (space-separated): ")
+        parents = np.fromstring(parents_str, sep=' ', dtype=int)
+        if len(parents) != n:
+            print("Error: invalid number of parent nodes!")
+            return
+        tree_height = compute_height(n, parents)
+        print(tree_height)
+    else:
+        print("Error: invalid input!")
         return
-    input_file = f"{task_num}"
-    if not os.path.exists(input_file):
-        print(f"fails {input_file} neeksistē")
-        return
-    with open(input_file, 'r') as f:
-        n = int(f.readline())
-        parents = np.fromstring(f.readline(), sep=' ', dtype=int)
-    tree_height = compute_height(n, parents)
-    print(tree_height)
 
 
 sys.setrecursionlimit(10**9)
